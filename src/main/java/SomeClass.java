da3199ed42edf3069fb0e3579ad00cb570d91f19
Jan Rotter <jan.rotter@softwaremill.pl>
2017-08-24 08:53:27 +0200

    Add a gradle project.

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,5 @@
+public class SomeClass {
+    int rollDice() {
+        return 5;
+    }
+}
---
+++ src/test/java/SomeClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SomeClassTest {
+    @Test
+    public void rollDice() throws Exception {
+        assertFalse(true);
+    }
+
+}
